5ef185ee487d5ce8309fb4bd12249985d7817090
harel.eran@gmail.com <harel.eran@gmail.com@28caf6ae-bbb4-a47b-a397-25366fdd89ce>
2011-02-08 13:10:25 +0000

    migrated the code to use jmemcached as the server side

--- src/main/java/com/outbrain/pajamasproxy/memcached/PajamasProxyDaemon.java
+++ src/main/java/com/outbrain/pajamasproxy/memcached/PajamasProxyDaemon.java
@@ -1,64 +1,15 @@
 package com.outbrain.pajamasproxy.memcached;
 
-import java.net.InetSocketAddress;
-
-import org.jboss.netty.bootstrap.ServerBootstrap;
-import org.jboss.netty.channel.Channel;
-import org.jboss.netty.channel.ChannelFuture;
-import org.jboss.netty.channel.socket.ServerSocketChannelFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.thimbleware.jmemcached.MemCacheDaemon;
+
 public class PajamasProxyDaemon {
-  private static final Logger log = LoggerFactory.getLogger(PajamasProxyDaemon.class);
 
-  private Channel inboundChannel;
-
-  private final ServerBootstrap serverBootstrap;
-
-  private final ServerSocketChannelFactory serverSocketChannelFactory;
-
-  private final int localPort;
-
-  private transient boolean running = false;
-
-  public PajamasProxyDaemon(final ServerBootstrap serverBootstrap, final ServerSocketChannelFactory serverSocketChannelFactory, final int localPort) {
-    this.serverBootstrap = serverBootstrap;
-    this.serverSocketChannelFactory = serverSocketChannelFactory;
-    this.localPort = localPort;
-  }
-
-  public void start() {
-    final InetSocketAddress socketAddress = new InetSocketAddress(localPort);
-    inboundChannel = serverBootstrap.bind(socketAddress);
-
-    running = true;
-    log.info("Pajamas Proxy Daemon Server started on address {}", socketAddress);
-  }
-
-  public void stop() {
-    log.info("Shutting down Pajamas Proxy Daemon Server");
-
-    final ChannelFuture closeOpFuture = inboundChannel.close();
-    closeOpFuture.awaitUninterruptibly();
-
-    if (closeOpFuture.isSuccess()) {
-      serverSocketChannelFactory.releaseExternalResources();
-      log.info("Server shut down completed successfully");
-    } else {
-      log.error("Failed to close all network channels");
-    }
-
-    running = false;
-  }
-
-  public boolean isRunning() {
-    return running;
-  }
+  //  private static final Logger log = LoggerFactory.getLogger(PajamasProxyDaemon.class);
 
   public static void main(final String[] args) {
-    final PajamasProxyDaemon daemon = new ClassPathXmlApplicationContext("ApplicationContext.xml").getBean(PajamasProxyDaemon.class);
+    final MemCacheDaemon<?> daemon = new ClassPathXmlApplicationContext("ApplicationContext.xml").getBean(MemCacheDaemon.class);
     daemon.start();
 
     Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
