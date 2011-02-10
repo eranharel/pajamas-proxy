56edfd97f6ed3955a6cb359d2bb69e2470b1a756
harel.eran@gmail.com <harel.eran@gmail.com@28caf6ae-bbb4-a47b-a397-25366fdd89ce>
2011-02-10 14:58:43 +0000

    added packaging as a single jar

--- trunk/start-pajamas.sh
+++ trunk/start-pajamas.sh
@@ -1,4 +1,1 @@
-mvn dependency:build-classpath -Dmdep.outputFile=classpath.txt
-export CP=`cat classpath.txt`
-
-java -cp $CP:target/pajamas-proxy-0.1-SNAPSHOT.jar -Dpajamas.properties=src/test/resources/pajamas-local.properties -Xmx2G com.outbrain.pajamasproxy.memcached.PajamasProxyDaemon
+java -server -cp target/pajamas-proxy-0.1-SNAPSHOT-jar-with-dependencies.jar -Dpajamas.properties=src/test/resources/pajamas-local.properties -Xmx1G com.outbrain.pajamasproxy.memcached.PajamasProxyDaemon
