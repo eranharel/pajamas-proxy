b31931e2641019f6f319817eec8cfeba38cad404
harel.eran@gmail.com <harel.eran@gmail.com@28caf6ae-bbb4-a47b-a397-25366fdd89ce>
2011-02-20 14:10:48 +0000

    removed xmemcached client usage

--- trunk/src/main/java/com/outbrain/pajamasproxy/memcached/proxy/CacheClientFactory.java
+++
@@ -1,7 +0,0 @@
-package com.outbrain.pajamasproxy.memcached.proxy;
-
-import net.rubyeye.xmemcached.MemcachedClient;
-
-public interface CacheClientFactory {
-  public MemcachedClient createCacheClient();
-}
