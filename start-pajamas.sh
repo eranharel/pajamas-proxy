6825fe4028f85ddae065d0015b3e3d2efb11fd7e
harel.eran@gmail.com <harel.eran@gmail.com@28caf6ae-bbb4-a47b-a397-25366fdd89ce>
2011-02-20 14:10:48 +0000

    removed xmemcached client usage

--- start-pajamas.sh
+++ start-pajamas.sh
@@ -1,1 +1,1 @@
-java -server -cp target/pajamas-proxy-0.1-SNAPSHOT-jar-with-dependencies.jar -Dpajamas.properties=src/test/resources/pajamas-local.properties -Xmx1G com.outbrain.pajamasproxy.memcached.PajamasProxyDaemon
+java -server -cp target/pajamas-proxy-0.1-SNAPSHOT-jar-with-dependencies.jar -Dpajamas.properties=src/test/resources/pajamas.properties -Xmx1G com.outbrain.pajamasproxy.memcached.PajamasProxyDaemon
