e54551df1f073e76b7a0ac57233f56b98d3799fa
harel.eran@gmail.com <harel.eran@gmail.com@28caf6ae-bbb4-a47b-a397-25366fdd89ce>
2011-03-09 09:34:11 +0000

    added docs

--- trunk/src/main/java/com/outbrain/pajamasproxy/memcached/monitor/StatisticsMBean.java
+++ trunk/src/main/java/com/outbrain/pajamasproxy/memcached/monitor/StatisticsMBean.java
@@ -1,15 +1,33 @@
 package com.outbrain.pajamasproxy.memcached.monitor;
 
+/**
+ * Aggregates the proxy statistics metrics.
+ * 
+ * @author Eran Harel
+ */
 public interface StatisticsMBean {
+
   public int getCurrentConnectionCount();
 
   public int getTotalConnectionCount();
 
+  /**
+   * @return The cache get commands count for the entire proxied cluster.
+   */
   public int getGetCommands();
 
+  /**
+   * @return The cache set commands count for the entire proxied cluster.
+   */
   public int getSetCommands();
 
+  /**
+   * @return The cache get hit count for the entire proxied cluster.
+   */
   public int getGetHits();
 
+  /**
+   * @return The cache get miss count for the entire proxied cluster.
+   */
   public int getGetMisses();
 }
