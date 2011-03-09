b1427dd0c8ffd94f35272e555ddabf3c57b43510
harel.eran@gmail.com <harel.eran@gmail.com@28caf6ae-bbb4-a47b-a397-25366fdd89ce>
2011-03-09 12:03:49 +0000

    removed obsolete startup script

--- trunk/start-pajamas.sh
+++
@@ -1,1 +0,0 @@
-java -server -cp target/pajamas-proxy-0.1-SNAPSHOT-jar-with-dependencies.jar -Dpajamas.properties=src/test/resources/pajamas.properties -Xmx1G com.outbrain.pajamasproxy.memcached.PajamasProxyDaemon
