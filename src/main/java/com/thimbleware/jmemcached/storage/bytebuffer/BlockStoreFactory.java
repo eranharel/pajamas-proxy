222033660e23e803d6258cb2fbef3e06d35b805a
harel.eran@gmail.com <harel.eran@gmail.com@28caf6ae-bbb4-a47b-a397-25366fdd89ce>
2011-04-06 08:52:14 +0000

    removed dead code

--- src/main/java/com/thimbleware/jmemcached/storage/bytebuffer/BlockStoreFactory.java
+++
@@ -1,7 +0,0 @@
-package com.thimbleware.jmemcached.storage.bytebuffer;
-
-/**
- */
-public interface BlockStoreFactory<BS extends ByteBufferBlockStore> {
-    BS manufacture(long sizeBytes, int blockSizeBytes);
-}
